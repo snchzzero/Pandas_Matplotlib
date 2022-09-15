3b2a22506e75ed903c6380c8dbfcc4e2ebf58241
snchzzero <aaevstafev@gmail.com>
2022-09-16 01:51:17 +0300

    data_analyst(2)

--- pnds_mtpltlb/data_analyst.py
+++ pnds_mtpltlb/data_analyst.py
@@ -7,6 +7,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import csv
 import pandas as pd
 import json
+import time
 
 
 
@@ -17,7 +18,7 @@ import requests
 import xmltodict
 
 # функция чтения гугл таблицы
-def google_API():
+def google_API_get():
     # не забываем расшарить сам документ в гугл таблицах, добавить пользователя из файлы creds.json
     #"client_email": "acountsnchzzero@radiant-planet-353422.iam.gserviceaccount.com",
 
@@ -26,13 +27,13 @@ def google_API():
     CREDENTIALS_FILE = 'creds.json'  # файл с API
     spreadsheet_id = '18Pzfrg0VEoHBcZqB19Nl7SrcSZ4ivOnQgAQOfYCwe30'  # из url схемы таблицы гугл (Pandas_Matplotlib)
 
-    # документы с которыми будем работать
+    # документы с которыми будем работать (читаем ключи из файла)
     creadentials = ServiceAccountCredentials.from_json_keyfile_name(
         CREDENTIALS_FILE,
         ['https://www.googleapis.com/auth/spreadsheets',
          'https://www.googleapis.com/auth/drive'])
 
-    # создаем объект аунтификации
+    # создаем объект аунтификации (авторизуемся в системе)
     httpAuth = creadentials.authorize(httplib2.Http())
 
     # создаем обертку API из которой мы будем получать данные из нашей схемы (v4 версия API sheets)
@@ -50,12 +51,10 @@ def google_API():
     # with open('data.json', 'w') as f:
     #     json.dump(values, f)
 
-
-
 def csv_data():
     with open('csv_data.csv', 'w') as f:
         writer = csv.writer(f)
-        data = google_API()
+        data = google_API_get()
         for row in data['values']:
             writer.writerow(row)
         print("csv_data успешно")
@@ -71,52 +70,178 @@ def analyst_data():
     pd.set_option('display.max_colwidth', None)
 
     data = pd.read_csv('csv_data.csv', encoding="Windows-1251")  # encoding для чтения русских символов
-    #print(data.info())
     data.insert(8, 'color', None)  # добавляем столбец color
-    # print(data.info())
-    # data.head()
-    # print("------------")
-    # print(data)
-    # for row in data.itertuples():
-    #     print(row)
-    # print()
-    # print()
-
-    d1 = dict()  # для предварительной записи color, что бы не повторялись
+    d1 = dict()  # для предварительной записи color, keyword, что бы избежать повторов
     data['count'] = pd.to_numeric(arg=data['count'], errors='coerce', downcast='integer')  # преобразования аргумента(string) в числовую форму
 
-    for row in data.itertuples():  # добавляем в колнку color цвет в соответствии с условием
+    # добавляем в колнку color цвет в соответствии с условием
+    # удаляем повтор keyword в одной области area
+    for row in data.itertuples():
         #print(row)  # вывести кортеж строки
         if row[1] not in d1:  # если area нет в словаре, то добавляем его и его первый класер + цвет
             col = random.choice(color)
-            d1[row[1]] = {row[2]: col}
+            d1[row[1]] = {row[2]: col, 'keyword':[row[4]]}
             #print(sort_data['color'].iloc[row[0]])  # вывести значение цвета (столбец и индекс)
             data.loc[row[0], 'color'] = col
+
             #print(data['color'].iloc[row[0]])  # получить значение из ячейки
             #print(data.iloc[row[0]])  # получить значение из строки
+
+        elif row[1] in d1 and row[4] in d1[row[1]]['keyword']:
+            data = data.drop(index=row[0])
         elif row[1] in d1 and row[2] in d1[row[1]]:  # если есть аrеа и есть кластер, то просто прибавляем цвет предыдущего кластера
             data.loc[row[0], 'color'] = d1[row[1]][row[2]]
+            d1[row[1]]['keyword'].append(row[4])
             #print(data.iloc[row[0]])  # получить значение из строки
         elif row[1] in d1 and row[2] not in d1[row[1]]:  # если есть area но нет кластера, прибавляем кластер + новый цвет
             col = random.choice(color)
             while col in d1[row[1]].values():
                 col = random.choice(color)
             d1[row[1]][row[2]] = col
+            d1[row[1]]['keyword'].append(row[4])
             data.loc[row[0], 'color'] = col
             #print(data.iloc[row[0]])  # получить значение из строки
 
-    # sort_data = data.sort_values(by=['area', 'cluster', 'cluster_name', 'count'],
-    #                              ascending=[True, True, True, False])  # сортировка по столбцам
-    for row in sort_data.itertuples():
-        print(row)
+    sort_data = data.sort_values(by=['area', 'cluster', 'cluster_name', 'count'],
+                                 ascending=[True, True, True, False])  # сортировка по столбцам
+    # sort_data = data.sort_values(by=['keyword', 'count'],
+    #                              ascending=[True, False])  # сортировка по столбцам (проверка на дублирующие слова)
+    # for row in sort_data.itertuples():
+    #     print(row)
+
+    return sort_data
+
+
+def google_API_send():
+    # не забываем расшарить сам документ в гугл таблицах, добавить пользователя из файлы creds.json
+    #"client_email": "acountsnchzzero@radiant-planet-353422.iam.gserviceaccount.com",
+
+    # подключение API
+
+    CREDENTIALS_FILE = 'creds.json'  # файл с API
+    spreadsheet_id = '19WU-rV31bktdBMZR1RM-ODJuuHamwcKaplPAJ-LEItY'  # из url схемы таблицы гугл (Pandas_Matplotlib)
+
+    # документы с которыми будем работать (читаем ключи из файла)
+    creadentials = ServiceAccountCredentials.from_json_keyfile_name(
+        CREDENTIALS_FILE,
+        ['https://www.googleapis.com/auth/spreadsheets',
+         'https://www.googleapis.com/auth/drive'])
+
+    # создаем объект аунтификации (авторизуемся в системе)
+    httpAuth = creadentials.authorize(httplib2.Http())
+
+    # создаем обертку API из которой мы будем получать данные из нашей схемы (v4 версия API sheets)
+    service = discovery.build('sheets', 'v4', http=httpAuth)
+
+    def del_tab():
+        # сначала полностью очищаем лист
+        rangeAll = '{0}!A1:Z'.format("Лист1")
+        body = {}
+        resultClear = service.spreadsheets().values().clear(spreadsheetId=spreadsheet_id, range=rangeAll,
+                                                            body=body).execute()
+
+    def tab_column_siz(startI, endI, size):
+        results = service.spreadsheets().batchUpdate(spreadsheetId=spreadsheet_id, body={"requests": [
+            {"updateDimensionProperties":
+                {
+                "range":
+                    {"sheetId": 0,
+                     "dimension": "COLUMNS",  # Задаем ширину колонки
+                     "startIndex": startI,  # Нумерация начинается с нуля
+                     "endIndex": endI  # Со столбца номер startIndex по endIndex - 1 (endIndex не входит!)
+                     },
+                "properties": {"pixelSize": size  # Ширина в пикселях
+                                },
+                "fields": "pixelSize"  # Указываем, что нужно использовать параметр pixelSize
+                }
+            }]}).execute()
+
+    def tab_text_format_main(startRowIndex=0, endRowIndex=0, startColumnIndex=0, endColumnIndex=0, bold=False, fontSize=10 ):
+        # Установка формата ячеек
+        results = service.spreadsheets().batchUpdate(
+            spreadsheetId=spreadsheet_id,
+            body=
+            {
+                "requests":
+                    [
+                        {
+                            "repeatCell":
+                                {
+                                    "cell":
+                                        {
+                                            "userEnteredFormat":
+                                                {
+                                                    "horizontalAlignment": 'CENTER',
+                                                    "textFormat":
+                                                        {
+                                                            "bold": bold,
+                                                            "fontSize": fontSize
+                                                        }
+                                                }
+                                        },
+                                    "range":
+                                        {
+                                            "sheetId": 0,
+                                            "startRowIndex": startRowIndex,
+                                            "endRowIndex": endRowIndex,
+                                            "startColumnIndex": startColumnIndex,
+                                            "endColumnIndex": endColumnIndex
+                                        },
+                                    "fields": "userEnteredFormat"
+                                }
+                        }
+                    ]
+            }).execute()
+
+    def tab_send_data():
+        data = analyst_data()
+        # заносим данные в таблицу
+        results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet_id, body={
+            "valueInputOption": "USER_ENTERED",
+            # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+            "data": [
+                {"range": "A1:G1",
+                 "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+                 "values": [['area', 'cluster', 'cluster_name', 'keyword', 'x', 'y', 'color'],  # Заполняем первую строку
+                 ]}
+            ]
+        }).execute()
+
+        total = 2
+        for row in data.itertuples():
+            results = service.spreadsheets().values().batchUpdate(spreadsheetId=spreadsheet_id, body={
+                "valueInputOption": "USER_ENTERED",
+                # Данные воспринимаются, как вводимые пользователем (считается значение формул)
+                "data": [
+                    {"range": f"A{total}:G{total}",
+                     "majorDimension": "ROWS",  # Сначала заполнять строки, затем столбцы
+                     "values": [
+                         [f'{row[1]}', f'{row[2]}', f'{row[3]}', f'{row[4]}', f'{row[7]}', f'{row[8]}', f'{row[9]}'],  # Заполняем первую строку
+
+                     ]}
+                ]
+            }).execute()
+            total += 1
+            if total % 50 == 0:  # API quota limitations из-за ограничения делаем паузу на 60сек после каждых 50 запросов
+                time.sleep(60)
+                continue
+        return total
+
+    del_tab()
+    tab_column_siz(0, 2, 80)
+    tab_column_siz(3, 4, 300)
+    tab_column_siz(4, 6, 150)
+    tab_column_siz(6, 7, 80)
+    tab_text_format_main(0, 1, 0, 7, True, 10)
+    endRowIndex = tab_send_data()
+    tab_text_format_main(1, endRowIndex, 0, 7, False, 9)
 
 
 
 
-    #print(data)
 
 
-csv_data()
-analyst_data()
+#csv_data()
+#analyst_data()
+google_API_send()
 
-#google_API()
